d3fad44b5a31f0b8736a04d3f0c5ab0e49a8fbc7
Emiliano Boccio <emi_boccio@outlook.com>
2022-10-26 01:43:19 -0300

    modifico nombres y carpetas

---
+++ Modulo6Unidad2/routes/admin/novedades.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
+
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre, 
+        novedades 
+    });
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades');
+});
+
+router.get('/agregar', async (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout',
+    }); //cierra render
+}); //cierra get
+
+/*insertar la novedad en la tabla*/ 
+
+router.post('/agregar', async (req, res, next) =>{
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')            
+        } else {
+            res.render ('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+        
+    }
+})
+
+/* para listar una sola novedad - by id */
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    console.log(req.params.id);
+    var novedad = await novedadesModel.getNovedadById(id);
+    
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+
+});
+
+/*MODIFICANDO LA NOVEDAD */
+router.post('/modificar', async (req, res, next) => {
+    try {
+       var obj = {
+        titulo: req.body.titulo,
+        subtitulo: req.body.subtitulo,
+        cuerpo: req.body.cuerpo
+       }
+
+       console.log(obj)
+       await novedadesModel.modificarNovedadById(obj, req.body.id);
+       res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+
+    }
+})
+
+module.exports = router;
---
+++ Tareas entregadas (Archivadas)/Modulo5Unidad4(sesiones)/routes/blog.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
+    res.render('blog', {
+        isNovedades: true,
+        novedades
+    });
+});
+
+module.exports = router;
